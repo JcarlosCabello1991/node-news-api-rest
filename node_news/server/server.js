b48c9df20c427769961595583eb0a61853f5ad19
JcarlosCabello1991 <jc.cabellomangas6@gmail.com>
2022-09-10 20:04:04 +0200

    fix api key

--- node_news/server/server.js
+++ node_news/server/server.js
@@ -2,19 +2,22 @@ import fetch from 'node-fetch'
 import express from 'express'
 import cors from 'cors'
 import fs from 'node:fs'
+import dotenv from 'dotenv'
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
 
 app.get("/getNews", (req, res, next) => {
- fs.readFile("./newsDB.json",'utf-8', (err, data)=>{
+ fs.readFile("/newsDB.json",'utf-8', (err, data)=>{
   //If we get an error reading file
   //we make an API call to get the news
   //If not we save data in a file to
   //prevent another API call innnecesary
     if(err){
       try{
-        fetch("https://newsdata.io/api/1/news?apikey=pub_11090d2e9b96f54bf4ea794d27af385132c56&language=es")
+        fetch(`https://newsdata.io/api/1/news?apikey=${process.env.API_KEY}&language=es`)
         .then(response => response.json())
         .then(data => {
           
@@ -23,7 +26,8 @@ app.get("/getNews", (req, res, next) => {
               throw err
             }
           })
-          res.send(data)
+          console.log(data);
+          res.send(data.results)
         })
       }catch(error){
         next(error)
